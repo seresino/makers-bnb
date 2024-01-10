7dec5460ad5390de061dd05f61ba4f2916e27cd6
dan gullis <daniel.gullis1@gmail.com>
2024-01-10 12:21:24 +0000

    added class for availability table, added working test for availability class

---
+++ lib/availability.py
@@ -0,0 +1,18 @@
+import os
+from peewee import *
+from lib.listing import Listing
+from dotenv import load_dotenv
+
+load_dotenv()
+db_username = os.getenv('DB_USERNAME')
+
+db = PostgresqlDatabase('makersbnb-red-team', user=db_username, password='', host='localhost')
+
+class Availability(Model):
+    listing = ForeignKeyField(Listing, backref='listing')
+    start_date = DateField()
+    end_date = DateField()
+    status = CharField()
+
+    class Meta:
+        database = db 
