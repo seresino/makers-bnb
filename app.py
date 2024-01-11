c27af581fc3be29db8b20e5e5597a72e2f5c4b12
dan gullis <daniel.gullis1@gmail.com>
2024-01-11 15:07:19 +0000

    created utils file and moved availability checking function there

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import json
 from flask_bcrypt import Bcrypt
 from lib.booking import *
 from datetime import datetime
-
+from utils import *
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -67,19 +67,6 @@ def after_request(response):
     db.close()
     return response
 
-
-def check_availability_overlap(availabilities, new_start_date, new_end_date):
-    for availability in availabilities:
-        existing_start_date = availability.start_date
-        existing_end_date = availability.end_date
-
-        if (
-            (existing_start_date <= new_start_date <= existing_end_date) or
-            (existing_start_date <= new_end_date <= existing_end_date) or
-            (new_start_date <= existing_start_date and new_end_date >= existing_end_date)
-        ):
-            return True
-    return False
 
 
 # == Your Routes Here ==
